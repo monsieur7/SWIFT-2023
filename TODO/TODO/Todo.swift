9e56df521dfd55bb6032691b6acf8c394e339ffc
nolane <nolane.de@gmail.com>
2023-11-28 15:55:27 +0100

    Adding class TODO

---
+++ TODO/TODO/Todo.swift
@@ -0,0 +1,26 @@
+//
+//  Todo.swift
+//  TODO
+//
+//  Created by Nolane Delille on 28/11/2023.
+//
+
+import Foundation
+enum TodoType {
+    case DEVOIRS;
+    
+}
+class Todo {
+    
+    var title: String;
+    var description: String;
+    var type: TodoType;
+    init(){
+        title = "";
+        description = "";
+        type = TodoType.DEVOIRS;
+    }
+    
+    
+    
+}
