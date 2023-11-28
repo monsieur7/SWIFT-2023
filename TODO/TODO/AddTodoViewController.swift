2de61eff5b53aa95a404899d313e03b528b6fda5
nolane <nolane.de@gmail.com>
2023-11-28 17:28:11 +0100

    work final 1 seance

---
+++ TODO/TODO/AddTodoViewController.swift
@@ -0,0 +1,29 @@
+//
+//  AddTodoViewController.swift
+//  TODO
+//
+//  Created by Nolane Delille on 28/11/2023.
+//
+
+import UIKit
+
+class AddTodoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+//https://manasaprema04.medium.com/different-ways-to-pass-data-between-viewcontrollers-views-8b7095e9b1bf
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
