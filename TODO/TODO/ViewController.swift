d8e89b42dc3bd3b2b8d9d31d5350866fa4fef94c
nolane <nolane.de@gmail.com>
2023-11-28 16:55:50 +0100

    work in viewController

--- TODO/TODO/ViewController.swift
+++ TODO/TODO/ViewController.swift
@@ -13,13 +13,22 @@ class ViewController: UIViewController, UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        return
+        let dataCell = data[indexPath.row];
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ID") as! TableViewCell;
+        cell.LabelTitle.text = dataCell.title;
+        cell.label_description.text = dataCell.description;
+        return cell;
     }
     
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        let testTodo = Todo(title: "Hello", description: "Hey", type: TodoType.DEVOIRS, dueDate: Date(), date: Date());
+        data.append(testTodo);
+        data.append(testTodo);
+        
+        self.tableview.dataSource = self;
     }
 
     
